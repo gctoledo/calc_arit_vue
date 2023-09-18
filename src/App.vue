7c0cb6585a63c82942530e5a1c9faec7e7a5feb9
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-09-18 13:42:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { reactive } from 'vue';
+import Calculadora from './components/Calculadora.vue'
+
+const state = reactive({
+  result: 0,
+  num1: 0,
+  num2: 0,
+  operator: '+'
+});
+
+const calc = () => {
+  switch (state.operator) {
+    case '+':
+      state.result = +state.num1 + +state.num2
+      break;
+    case '-':
+      state.result = +state.num1 - +state.num2
+      break;
+    case '/':
+      state.result = +state.num1 / +state.num2
+      break;
+    case '*':
+      state.result = +state.num1 * +state.num2
+      break;
+  }
+}
+
+const getNumber = e => {
+  if (e.target.id == 'num1') {
+    state.num1 = e.target.value;
+  } else {
+    state.num2 = e.target.value
+  }
+  calc();
+}
+
+const getOperator = e => {
+  state.operator = e.target.value;
+  calc();
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Calculadora</h1>
+    <Calculadora :result="state.result" :get-number="getNumber" :get-operator="getOperator" />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+  font-weight: bold;
+  color: #424242;
+}
+
+.container {
+  width: 1024px;
+  max-width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 150px;
+  padding: 20px;
+  background-color: #e6e6e6;
+  border-radius: 5px;
+}
+
+.container h1 {
+  font-size: 32px;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+}
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps(['getNumber', 'getOperator', 'result'])
+</script>
+
+<template>
+    <div class="calc">
+        <input type="number" @keyup="props.getNumber" id="num1" placeholder="Valor 1">
+        <select @change="props.getOperator">
+            <option selected value="+">+</option>
+            <option value="-">-</option>
+            <option value="/">/</option>
+            <option value="*">*</option>
+        </select>
+        <input type="number" @keyup="props.getNumber" id="num2" placeholder="Valor 2">
+        <span class="result">{{ props.result }}</span>
+    </div>
+</template>
+
+<style scoped>
+input,
+select {
+    padding: 12px;
+    border-radius: 5px;
+}
+
+select {
+    margin: 0 10px 0 10px;
+}
+
+.result {
+    display: block;
+    margin-top: 32px;
+    font-size: 24px;
+}
+</style>
